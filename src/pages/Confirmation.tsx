46b76503387d498e9ae1e0eac26ed5bceb73689e
Vitor Valandro <vitormateusd@gmail.com>
2021-04-22 13:36:48 -0300

    Day 3: create Header component

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/core';
 import { SafeAreaView, StyleSheet, View, Text, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
 
 import { Button } from '../components/Button';
@@ -6,6 +7,11 @@ import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
 export function Confirmation(){
+  const navigation = useNavigation();
+
+  function handleMoveOn(){
+    navigation.navigate("PlantSelect");
+  }
   return(
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
@@ -20,7 +26,7 @@ export function Confirmation(){
             suas plantinhas com muito cuidado.
           </Text>
         <View style={styles.footer}>
-          <Button title={"Começar"} />
+          <Button title={"Começar"} onPress={handleMoveOn}/>
         </View>
       </View>
     </SafeAreaView>
--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -3,11 +3,21 @@ import { View, Text, StyleSheet } from 'react-native';
 
 import colors from '../styles/colors';
 import {Header} from '../components/Header';
+import fonts from '../styles/fonts';
 
 export function PlantSelect(){
   return(
     <View style={styles.container}>
-      <Header />
+      <View style={styles.header}>
+        <Header />
+
+        <Text style={styles.title}>
+          Em qual ambiente
+        </Text>
+        <Text style={styles.subtitle}>
+          você quer colocar sua planta
+        </Text>
+      </View>
     </View>
   )
 }
@@ -16,5 +26,21 @@ const styles = StyleSheet.create({
   container:{
     flex:1,
     backgroundColor: colors.background,
+  },
+  title:{
+    fontSize: 17,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 20,
+    marginTop: 15,
+  },
+  subtitle:{
+    fontFamily: fonts.text,
+    fontSize: 17,
+    lineHeight: 20,
+    color: colors.heading
+  },
+  header:{
+    paddingHorizontal: 30,
   }
 })
